64e52c01b900350efe3198d08b42222934646598
sdw2330976 <sydhappy123@163.com>
2014-04-12 19:42:51 +0800

    Spring MVC exercise

---
+++ titanium-core/src/main/java/com/sdw/soft/titanium_core/App.java
@@ -0,0 +1,13 @@
+package com.sdw.soft.titanium_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
