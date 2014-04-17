99f48e1024d8a5d2ce49d5fe8cbaa134f4f3b1a3
sdw2330976 <sydhappy123@163.com>
2014-04-17 20:13:28 +0800

    内容协商管理器 negotiationManager

--- titanium-showcase/src/main/java/com/sdw/soft/test/web/action/HelloController.java
+++ titanium-showcase/src/main/java/com/sdw/soft/test/web/action/HelloController.java
@@ -1,5 +1,8 @@
 package com.sdw.soft.test.web.action;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,16 @@ public class HelloController {
 	@RequestMapping(value="hello")
 	public String hello(Model model){
 		model.addAttribute("hello", "hello,Spring MVC...");
+		return "hello";
+	}
+	@RequestMapping(value="hello2")
+	public String helloJson(Model model){
+		Map<String,String> messages = new HashMap<String,String>();
+		messages.put("id", "123");
+		messages.put("username", "Tom");
+		messages.put("password", "admin123");
+		messages.put("email", "test@test.com");
+		model.addAttribute("messages", messages);
 		return "hello";
 	}
 }
--- titanium-showcase/src/main/webapp/WEB-INF/pages/hello.jsp
+++ titanium-showcase/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,10 +1,18 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ include file="/common/taglibs.jsp" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="${base }/static/js/jquery/jquery-1.9.1.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$.getJSON("${base}/hello/hello2.json",{},function(data){
+			alert(JSON.stringify(data));
+		});
+	});
+</script>
 </head>
 <body>
 	hello ! --- ${hello }
